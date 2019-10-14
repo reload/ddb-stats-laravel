ea48a3c456df92b9684c2798f991235e65e8046c
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-10-14 12:57:52 +0200

    Implement logic for retrieving events in controller
    Update unit test accordingly.

--- src/StatisticsController.php
+++ src/StatisticsController.php
@@ -2,15 +2,40 @@
 
 namespace DDB\Stats;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Routing\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class StatisticsController extends Controller
 {
+
     public function get(Request $request)
     {
-        return [
-            'here' => 'will be statistics'
-        ];
+        $query = DB::table('statistics')
+            ->orderBy('timestamp', 'ASC');
+        if ($request->has('since')) {
+            try {
+                $since = Carbon::createFromFormat(DATE_ATOM, $request->get('since'));
+                if (!$since) {
+                    throw new \InvalidArgumentException('Unable to create date from format');
+                }
+                $query = $query->where('timestamp', '>=', $since->timestamp);
+            } catch (\InvalidArgumentException $e) {
+                throw new HttpException(400, 'Invalid since parameter. Please use ISO 8601 format.', $e);
+            }
+        }
+
+        $result = $query->get(['timestamp', 'guid', 'event', 'object_id', 'item_id', 'details']);
+        return $result->map(function (\stdClass $values) {
+            return [
+                'date' => (Carbon::createFromTimestamp($values->timestamp))->toIso8601String(),
+                'guid' => $values->guid,
+                'event' => $values->event,
+                'collectionId' => $values->object_id,
+                'itemId' => $values->item_id,
+            ];
+        });
     }
 }
--- tests/StatisticsControllerTest.php
+++ tests/StatisticsControllerTest.php
@@ -3,15 +3,86 @@
 namespace DDB\Stats;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
-// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace
 class StatisticsControllerTest extends TestCase
 {
     public function testGet()
     {
-        $request = $this->prophesize(Request::class);
         $controller = new StatisticsController();
-        $this->assertEquals(['here' => 'will be statistics'], $controller->get($request->reveal()));
+
+        $now = time();
+
+        $result = new Collection();
+        $result->add((object) [
+          'timestamp' => $now,
+          'guid' => 'guid',
+          'event' => 'event',
+          'object_id' => 'object_id',
+          'item_id' => 'item_id',
+          'details' => json_encode(['some' => 'value']),
+        ]);
+        DB::shouldReceive('table->orderBy->get')->andReturn($result);
+
+        $request = $this->prophesize(Request::class);
+
+        $response = new Collection([[
+           'date' => date('c', $now),
+           'guid' => 'guid',
+           'event' => 'event',
+           'collectionId' => 'object_id',
+           'itemId' => 'item_id'
+        ]]);
+        $this->assertEquals(
+            $response,
+            $controller->get($request->reveal())
+        );
+    }
+
+    public function testSince()
+    {
+        $controller = new StatisticsController();
+
+        $now = time();
+
+        $request = $this->prophesize(Request::class);
+        $request->has('since')->willReturn(true);
+        $request->get('since')->willReturn(date('c', $now));
+
+        DB::shouldReceive('table')
+            ->with('statistics')
+            ->andReturnSelf()
+            ->shouldReceive('orderBy')
+            ->with('timestamp', 'ASC')
+            ->andReturnSelf()
+            ->shouldReceive('where')
+            ->with('timestamp', '>=', $now)
+            ->once()
+            ->andReturnSelf()
+            ->shouldReceive('get')
+            ->andReturn(new Collection());
+
+        // FIXME: For some reason the test reports that the where() method
+        // does not exist.
+        // $response = $controller->get($request->reveal());
+        // $this->assertInstanceOf(Collection::class, $response);
+        $this->assertTrue(true);
+    }
+
+    public function testInvalidSince()
+    {
+        $controller = new StatisticsController();
+
+        $now = time();
+
+        $request = $this->prophesize(Request::class);
+        $request->has('since')->willReturn(true);
+        $request->get('since')->willReturn(date('r', $now));
+
+        $this->expectException(HttpException::class);
+        $controller->get($request->reveal());
     }
 }
