6bc8c8f0c0037326dd8b51ecdcef44389beba658
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-10-18 13:33:22 +0200

    Add support for Lumen

--- src/ServiceProviders/StatisticsEventsServiceProvider.php
+++ src/ServiceProviders/StatisticsEventsServiceProvider.php
@@ -1,17 +1,26 @@
-<?php
+<?php // phpcs:disable PSR1.Files.SideEffects
 
 namespace DDB\Stats\ServiceProviders;
 
 use DDB\Stats\Events\StatisticsClaimed;
 use DDB\Stats\Listeners\RemoveObsoleteStatistics;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 
-class StatisticsEventsServiceProvider extends EventServiceProvider
+// Laravel and Lumen have different base classes for event service providers.
+// If we detect a Lumen variant then use that class instead of the Laravel one.
+// That should be safe as long as we are using basic features.
+if (class_exists('\Laravel\Lumen\Providers\EventServiceProvider')) {
+    class_alias(
+        '\Laravel\Lumen\Providers\EventServiceProvider',
+        '\Illuminate\Foundation\Support\Providers\EventServiceProvider'
+    );
+}
+
+class StatisticsEventsServiceProvider extends \Illuminate\Foundation\Support\Providers\EventServiceProvider
 {
 
     protected $listen = [
-      StatisticsClaimed::class => [
-        RemoveObsoleteStatistics::class
-      ],
+        StatisticsClaimed::class => [
+            RemoveObsoleteStatistics::class
+        ],
     ];
 }
