385051f154b062983911bd12ef7d82a8727600ff
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-10-21 09:38:28 +0200

    Document statistics collector event function
    Parameter descriptions are mostly copy/pasted from the specification.

--- src/StatisticsCollector.php
+++ src/StatisticsCollector.php
@@ -5,6 +5,27 @@ namespace DDB\Stats;
 interface StatisticsCollector
 {
 
+    /**
+     * Register an event which occurred within the application.
+     *
+     * @param string|null $guid
+     *   The globally unique identifier for the user on behalf of who the event
+     *   is performed.
+     * @param string $event
+     *   The event name. Names use snake_case per convention.
+     * @param string|null $collection_id
+     *   The id for a collection of items. What constitues a collection of items
+     *   is defined by the system.
+     * @param string|null $item_id
+     *   The id for an item related to the event. What constitutes an item is
+     *   defined by the system.
+     * @param int|null $total_count
+     *   The total number of items/collections after the event has been
+     *   completed if relevant for the event.
+     * @param string[] $content
+     *   The ids within the collection after the completion of the event if
+     *   relevant for the event.
+     */
     public function event(
         ?string $guid,
         string $event,
